fd07a49a5ebbf647de2f7e391c2bf3ad1b9442cc
Andrew Helgeson <planemantheace@gmail.com>
2011-07-18 17:21:38 -0500

    More work to the physics manager.
    Still not runnable.
    
    Commit by: <planemantheace@gmail.com>

--- include/physics.h
+++ include/physics.h
@@ -1,6 +1,8 @@
 #ifndef PHYSICS_H_INCLUDED
 #define PHYSICS_H_INCLUDED
 
+#inlcude <vector>
+#include <list>
 #include "SDL/SDL.h"
 
 typedef struct {
@@ -12,6 +14,9 @@ typedef struct {
     int height;
 
     int level;
+
+    vector< pair<int,int> > grid_locations;
+    int rectID;
 } phys_rect;
 
 typedef class Particle {
@@ -31,13 +36,17 @@ typedef class Particle {
 class cPhysic_manager{
     private:
         int grid_w, grid_h;
-        phys_rect*** collision_zone_grid;
+        list<phys_rect*>** collision_zone_grid;
+        list<phys_rect*>** collision_obj_grid;
+
+        cID_dispatch obj_id_manage;
 
     public:
         cPhysic_manager(int grid_width, int grid_height, int levels);
         /* Sets up a collision zone so any colision objects cannot pass through it */
         /* If nothing is passed for level it will collide with objects across all levels */
         void PM_set_collide_zone(int x, int y, int width, int height, int level = -1);
+        void PM_register_collision_obj(phys_rect*);
         phys_rect** PM_merge_rects(phys_rect*,phys_rect*);
 };
 
--- src/physics.cpp
+++ src/physics.cpp
@@ -1,7 +1,22 @@
 #include "physics.h"
 
+#define VECTOR_GRID_INIT_SIZE 10
+
 cPhysic_manager::cPhysic_manager(int grid_width, int grid_height) {
-    collision_zone_grid = (phys_rect***) malloc( grid_width * grid_height * sizeof(phys_rect*) );
+    collision_zone_grid = (list<phys_rect*>**) malloc( grid_width * grid_height * sizeof(list<phys_rect*>*) );
+    collision_obj_grid = (list<phys_rect*>**) malloc( grid_width * grid_height * sizeof(vector<phys_rect*>*) );
+
+    for (int i = 0; i < grid_width; ++i) {
+        for (int j = 0; j < grid_height; ++j) {
+            collision_zone_grid = new list<phys_rect*>;
+            collision_obj_grid = new list<phys_rect*>(VECTOR_GRID_INIT_SIZE);
+            obj_grid_load[i][j] = 0;
+        }
+    }
+
+    obj_id_manage = cID_dispatch();
+    grid_w = grid_width;
+    grid_h = grid_height;
     return;
 }
 
@@ -12,7 +27,53 @@ void cPhysic_manager::PM_set_collide_zone(int x, int y, int width, int height, i
     new_rect->width = width;
     new_rect->height = height;
     new_rect->level = level;
-    collision_zone_grid[x % grid_w][y % grid_h]
+    collision_zone_grid[x / grid_w][y / grid_h];
+}
+
+void cPhysic_manager::PM_register_collision_obj(phys_rect* obj) {
+    int grid_x, grid_y;
+    phys_rect->grid_locations = vector< pair<int,int> >;
+    phys_rect->rectID = obj_id_manage.ID_getid();
+
+    //Grid index
+    grid_x = phys_rect->x / grid_w;
+    grid_y = phys_rect->y / grid_h;
+
+    printf("PM registered a phys_rect with ID: %d\n",phys_rect->rectID);
+    printf("\nScreen Location: <%d,%d>\n",phys_rect->x,phys_rect->y);
+    printf("\nGrid Location: <%d,%d>\n",grid_x,grid_y);
+
+    phys_rect->grid_locations[0] = pair<int,int>(grid_x,grid_y);
+
+    //How many grid sections an object spans
+    x_span = phys_rect->width / grid_w;
+    y_span = phys_rect->height / grid_h;
+
+    if ( (phys_rect->x % grid_w) == 0 ) {
+        for (int i = 1; (i < x_span) && (grid_x + i < grid_width); ++i) {
+            collision_obj_grid[grid_x + i][grid_y]->push_back(obj);
+            phys_rect->grid_locations.push_back( pair<int,int>(grid_x+i,grid_y) );
+        }
+    } else {
+        for (int i = 0; (i < x_span) && (grid_x + i < grid_width); ++i) {
+            collision_obj_grid[grid_x + i][grid_y]->push_back(obj);
+            phys_rect->grid_locations.push_back( pair<int,int>(grid_x+i,grid_y) );
+        }
+    }
+
+    if ( (phys_rect->y % grid_h) == 0) {
+        for (int j = 1; (i < y_span) && (grid_y + i < grid_height); ++j) {
+            collision_obj_grid[grid_x][grid_y + j]->push_back(obj);
+            phys_rect->grid_locations.push_back( pair<int,int>(grid_x,grid_y+j) );
+        }
+    } else {
+        for (int j = 0; (i < y_psan) && (grid_y + i < grid_height); ++ j) {
+            collision_obj_grid[grid_x][grid_y + ]->push_back(obj);
+            phys_rect->grid_locations.push_back( pair<int,int>(grid_x,grid_y+j) );
+        }
+    }
+
+    collision_obj_grid[grid_x][grid_y]->push_back(obj);
 }
 
 
